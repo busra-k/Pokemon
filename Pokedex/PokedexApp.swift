723c196f415c0384c94acf00020392cae241ff43
Büşra Kocakuşaklı <busrakocakusakli@gmail.com>
2023-07-01 20:23:06 +0300

    Pokemon API
    It is an app that lists the Pokemon pulled from the Pokemon API.

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Büşra Kocakuşaklı on 29.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pokedex/Views/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  Pokedex
+//
+//  Created by Büşra Kocakuşaklı on 29.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var pokemon = [PokemonEntry]()
+    @State var searchText = ""
+    var body: some View {
+        
+        
+        NavigationView {
+            List {
+                ForEach(searchText == "" ? pokemon : pokemon.filter( { $0.name.contains(searchText.lowercased())
+                    
+                    
+                })) { entry in
+                    HStack {
+                        PokemonImage(imageLink: "\(entry.url)")
+                            .padding(.trailing, 20)
+                        NavigationLink("\(entry.name)".capitalized, destination:  Text("Detail view for \(entry.name)"))
+                    }
+                }
+                
+            }
+            .onAppear {
+                PokeApi().getData() { pokemon in self.pokemon = pokemon
+                    
+                }
+            }
+            .searchable(text: $searchText)
+            .navigationTitle("PokedexUI")
+        }
+            
+        }
+        
+    }
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
